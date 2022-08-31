3fd9ac680c96ac486f7e9d8ac269dda34cb0a763
Francisco Araújo <franciscoaraujo2016f2@outlook.com>
2022-08-31 10:25:07 -0300

    initial commit

--- appgrafico.py
+++ appgrafico.py
@@ -7,29 +7,25 @@ with open("style.css") as p:
 st.title(":sunglasses:")
 
 st.sidebar.title('*Menu Inicial*')
-varpagina = st.sidebar.selectbox('Selecione a função para visualizar o gráfico:', [st.latex(r'''
-200 \times * \sqrt(2) \times \sin(2 \times \times \pi * 60 * t'''), st.latex(r'''(200 \times * \sqrt(2) \times \sin(2 \times \times \pi * 60 * t)^2''')])
-if varpagina == '200 \times * \sqrt(2) \times \sin(2 \times \times \pi * 60 * t':
+varpagina = st.sidebar.selectbox('Selecione a função para visualizar o gráfico:', [st.latex(r'''200 \times \sqrt(2) \times \sin(2 \times \times \pi \times 60 \times t'''), st.latex(r'''(200 \times \sqrt(2) \times \sin(2 \times \times \pi \times 60 \times t)^2''')])
+if varpagina == st.latex(r'''200 \times \sqrt(2) \times \sin(2 \times \times \pi \times 60 \times t'''):
     x = st.slider('Domínio', min_value=0.00000000001, max_value=0.1, step=0.00001)
     color = st.color_picker('Cores', '#00f900')
     t = np.linspace(0, x, 1000)
     v1 = 220 * np.sqrt(2) * np.sin(2 * np.pi * 60 * t)
     fig1 = go.Figure(data=go.Scatter(x=t, y=v1))
     fig1.data[0].line.color = color
-    st.markdown('*Função 1:*')
-    st.markdown('*220 * np.sqrt(2) * np.sin(2 * np.pi * 60 * t)*')
+    st.markdown(st.latex(r'''200 \times \sqrt(2) \times \sin(2 \times \times \pi \times 60 \times t'''))
     st.plotly_chart(fig1)
 
-elif varpagina == st.latex(r'''
-(200 \times * \sqrt(2) \times \sin(2 \times \times \pi * 60 * t)^2'''):
+elif varpagina == st.latex(r'''(200 \times \sqrt(2) \times \sin(2 \times \times \pi \times 60 \times t)^2'''):
     x = st.slider('Domínio', min_value=0.00000000001, max_value=0.1, step=0.00001)
     color = st.color_picker('Cores', '#D20A41')
     t = np.linspace(0, x, 1000)
     v2 = (220 * np.sqrt(2) * np.sin(2 * np.pi * 60 * t)) ** 2
     fig2 = go.Figure(data=go.Scatter(x=t, y=v2))
     fig2.data[0].line.color = color
-    st.markdown('*Função 2:*')
-    st.markdown('*(220 * np.sqrt(2) * np.sin(2 * np.pi * 60 * t)) ** 2*')
+    st.markdown(st.latex(r'''(200 \times \sqrt(2) \times \sin(2 \times \times \pi \times 60 \times t)^2'''))
     st.plotly_chart(fig2)
 
 """
